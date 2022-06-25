cddf88a013ec84d240740a4426a549871c8d61d0
Mounik <mounicou@gmail.com>
2022-06-25 22:43:02 +0200

    Ajout du scanner de ports

---
+++ Ze Scripts/scan_port.py
@@ -0,0 +1,29 @@
+import socket
+import termcolor
+
+def scan(target, ports):
+    print(termcolor.colored(('\n' + ' Starting Scan For ' + target + ': '), 'green'))
+    for port in range(1, ports):
+        scan_port(target, port)
+
+
+
+def scan_port(ipaddress, port):
+    try:
+        sock = socket.socket()
+        sock.connect((ipaddress, port))
+        print("[+] Port Opened " + str(port))
+        sock.close()
+    except:
+        pass
+
+
+targets = input("[*] Enter Targets To Scan (split them by ,): ")
+ports = int(input("[*] Enter How Many Ports Do You Want To Scan: "))
+if ',' in targets:
+    print(termcolor.colored(("[*] Scanning Multiple Targets"), 'red'))
+    for ip_addr in targets.split(','):
+        scan(ip_addr.strip(' '), ports)
+else:
+    print(termcolor.colored(("[*] Scanning Single Target"), 'red'))
+    scan(targets, ports)
